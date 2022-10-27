3886c839f830d20d40bce58692c618099efbd4e6
SorenHeidelbach <lx38ll@bio.aau.dk>
2022-10-27 14:29:13 +0200

    Removal of too convolutional functions

--- R/current_difference.R
+++ R/current_difference.R
@@ -11,30 +11,36 @@
 #' @return dt with calculated statistics
 #' @import data.table
 #' @export
-preprocess_all_chunks <- function(metainfo,
-                                  h5_list,
-                                  out,
-                                  chunk_size = 1e5,
-                                  ...) {
-  lapply(
-    seq_along(metainfo),
-    function(i) {
-      logger::log_debug((names(metainfo[i])))
-      chunk <- process_chunk(
-          metainfo[[i]],
-          h5_list = h5_list,
+preprocess_all_chunks <- function(
+  metainfo,
+  nat_hdf5,
+  pcr_hdf5,
+  out,
+  chunk_size = 1e5
+) {
+  future.apply::future_lapply(
+    metainfo,
+    function(x) {
+      # For future, worker nodes to copy in memory to acces in nested functions
+      x <- copy(x)
+      chunk_reference <- unique(x[[1]][[1]]$chunk_ref)
+      logger::log_debug(chunk_reference)
+      chunk_stats <- process_chunk(
+          x,
+          nat_hdf5 = nat_hdf5,
+          pcr_hdf5 = pcr_hdf5,
           chunk_size = chunk_size
         )
-
-      chunk[, chunk_ref := names(metainfo[i])]
-
-      chunk_out_path <- paste_path(out, "/chunks/", names(metainfo)[i])
+      rm(x)
+      gc()
+      chunk_stats[, chunk_ref := chunk_reference]
+      chunk_out_path <- paste_path(out, "/chunks/", chunk_reference)
       dir.create(chunk_out_path, recursive = TRUE)
-      fwrite(chunk, paste_path(chunk_out_path, "chunk.tsv"))
+      fwrite(chunk_stats, paste_path(chunk_out_path, "chunk.tsv"))
+      rm(chunk_stats)
     }
   )
 }
-
 #' Calculates statistics between NAT and PCR signal
 #'
 #' Calculates statistics between NAT and PCR signal
@@ -45,22 +51,37 @@ preprocess_all_chunks <- function(metainfo,
 #' @return current difference data.table
 #' @import data.table
 #' @export
-calculate_statistics <- function(signal,
-                               window = 8,
-                               value_col = "mean_diff"
-                               ) {
-    chunk_diff <- signal %>%
-      gather_nat_and_pcr() %>%
-      calculate_mean_dif() %>%
-      calculate_median_dif() %>%
-      add_frame_statistics(frame_size = window, value_col = value_col) %>%
-      remove_when_signal_missing() %>%
-      calculate_wilcox_two_sided()
-    chunk_diff[
-      , `:=`(nat = NULL, pcr = NULL)
-    ]
-    return(chunk_diff)
-  }
+calculate_statistics <- function(
+  signal,
+  frame_size = 8,
+  value_col = "mean_diff"
+) {
+  chunk_diff <- dcast(
+    signal,
+    strand + pos_ref ~ type,
+    value.var = "signal", fun.agg = function(x) list(unlist(x))
+  )
+  chunk_diff[
+    , mean_diff := mean(unlist(nat)) - mean(unlist(pcr), na.rm=TRUE),
+    by = .(pos_ref, strand)
+  ][
+    , median_diff := median(unlist(nat)) - median(unlist(pcr), na.rm=TRUE),
+    by = .(pos_ref, strand)
+  ][
+    , paste0(value_col, -frame_size:frame_size) := get_moving_cunks(get(value_col), frame_size = frame_size),
+    by = strand
+  ]
+  chunk_diff <- chunk_diff[
+    (lengths(nat) > 1) & (lengths(pcr) > 1)
+  ][
+    , p_val := wilcox.test(unlist(nat), unlist(pcr), exact = FALSE)$p.value,
+    by = .(pos_ref, strand)
+  ]
+  chunk_diff[
+    , `:=`(nat = NULL, pcr = NULL)
+  ]
+  return(chunk_diff)
+}
 
 #' Extract features from event statistics
 #'
@@ -71,9 +92,9 @@ calculate_statistics <- function(signal,
 #' @param p_value_threshold Threshold for event selection (based on two sided wilcox test)
 #' @return data.table with feature columns and reference position
 #' @export
-get_event_features <- function(signal, p_value_threshold = 1e-10) {
+get_event_features <- function(signal, p_value_threshold = 1e-6) {
   signal[
-    , event := min(p_val) < p_value_threshold, by = .(pos_ref, chunk_ref)
+    , event := min(p_val) < p_value_threshold, by = .(pos_ref)
     ]
   
   events <- gather_plus_and_minus_strand(signal)
@@ -91,12 +112,18 @@ get_event_features <- function(signal, p_value_threshold = 1e-10) {
 #' @return dt with calculated statistics
 #' @import data.table
 #' @export
-process_chunk <- function(chunk_list, 
-                          h5_list, 
-                          chunk_size, 
-                          plot_path = paste0("./current_plots/", unique(chunk_list[[1]][[1]]$chunk_ref), "/")) {
-  
-  add_signal_chunk(chunk_list, h5_list)
+process_chunk <- function(
+  chunk_list,
+  nat_hdf5,
+  pcr_hdf5,
+  chunk_size,
+  plot_path = paste0("./current_plots/", unique(chunk_list[[1]][[1]]$chunk_ref), "/")
+) {
+  hdf5_list <- list(
+    nat = hdf5r::H5File$new(nat_hdf5, mode = "r"),
+    pcr = hdf5r::H5File$new(pcr_hdf5, mode = "r")
+  )
+  add_signal_chunk(chunk_list, hdf5_list = hdf5_list)
   logger::log_debug("Reformating chunk dt")
   chunk <- rbindlist(unlist(chunk_list, recursive = FALSE))
   chunk <- get_reference_context(chunk, chunk_size)
@@ -111,43 +138,6 @@ process_chunk <- function(chunk_list,
   return(chunk_stats)
 }
 
-gather_nat_and_pcr <- function(chunk){
-  dcast(
-    chunk,
-    strand + pos_ref ~ type,
-    value.var = "signal", fun.agg = function(x) list(unlist(x))
-  )
-}
-
-remove_when_signal_missing <- function(chunk){
-  chunk[
-    (lengths(nat) > 0) & (lengths(pcr) > 0)
-  ]
-}
-
-calculate_mean_dif <- function(chunk){
-  chunk[
-    ,
-    mean_diff := mean(unlist(nat)) - mean(unlist(pcr)),
-    by = .(pos_ref, strand)
-  ]
-}
-
-calculate_median_dif <- function(chunk){
-  chunk[
-    ,
-    median_diff := median(unlist(nat)) - median(unlist(pcr)),
-    by = .(pos_ref, strand)
-  ]
-}
-
-calculate_wilcox_two_sided <- function(chunk){
-  chunk[
-    ,
-    p_val := wilcox.test(unlist(nat), unlist(pcr), exact = FALSE)$p.value,
-    by = .(pos_ref, strand)
-  ]
-}
 
 get_moving_cunks = function(vector, frame_size = 8) {
   # Return list of 2*window+1 vectors.
@@ -164,25 +154,18 @@ get_moving_cunks = function(vector, frame_size = 8) {
     data.table::transpose()
   }
 
-add_frame_statistics <- function(chunk, frame_size, value_col) {
-  chunk[
-      ,
-      paste0(value_col, "_dif_", -frame_size:frame_size) := get_moving_cunks(get(value_col), frame_size = frame_size),
-      by = strand
-    ]
-}
 
 gather_plus_and_minus_strand <- function(chunk) {
     chunk_merged = merge(
     chunk[
         event == TRUE & strand == "-",
       ][
-        , .SD, .SDcols = names(chunk) %like% "diff_|pos_ref|chunk_ref"
+        , .SD, .SDcols = names(chunk) %like% "diff|pos_ref|chunk_ref"
       ],
     chunk[
         event == TRUE & strand == "+",
       ][
-        , .SD, .SDcols = names(chunk) %like% "diff_|pos_ref|chunk_ref"
+        , .SD, .SDcols = names(chunk) %like% "diff|pos_ref|chunk_ref"
       ],
     by = c("pos_ref", "chunk_ref"),
     all = TRUE,
@@ -198,4 +181,3 @@ add_reference <- function(chunk, sequence) {
       strand == "-", base := get_complement_sequence(sequence[pos_ref])
     ]
 }
-
